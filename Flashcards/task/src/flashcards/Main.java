df0deb63c18796d289afff916924121a78dbc713
RICO5k <maciekqwertyzaqwsx@gmail.com>
2019-09-24 22:53:26 +0200

    compleated stage2

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,7 +1,20 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String card = scanner.nextLine();
+        String definition = scanner.nextLine();
+
+        String answer = scanner.nextLine();
+
+        if(answer.equals(definition)) {
+            System.out.println("Your answer is right");
+        } else {
+            System.out.println("Your answer is wrong...");
+        }
     }
 }
